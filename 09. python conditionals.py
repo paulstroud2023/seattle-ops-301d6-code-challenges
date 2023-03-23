abfd275680548f78987fab4811ae21d170b07503
Paul Stroud <paul.stroud2023@gmail.com>
2023-03-23 17:44:49 +0000

    code 09

---
+++ 09. python conditionals.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python 
+
+# Script: Ops 301 Class 09 Ops Challenge Solution
+# Author: Paul Stroud
+# Date of latest revision: 03/23/23
+# Purpose: Python conditionals
+
+
+# Main
+
+
+if 10 == 11: print("10 == 11: True")
+elif 10 != 11: print("10 == 11: False")
+else: print("Is this real life?")
+
+if 5 < -5: print("5 < -5: True")
+elif -5 <= -5: print("-5 <= -5: True")
+else: print("Math is a social construct 🤡")
+
+if 55.555 > 55.554: print("55.555 > 55.554: True")
+if 55.555 >= 55.554: print("55.555 > 55.555: True")
+
+
+
+# End
