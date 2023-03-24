92a714a37ca981f982a35564c74e0f4130978db3
Paul Stroud <paul.stroud2023@gmail.com>
2023-03-24 17:49:06 +0000

    added file deletion

--- 10. python file handling.py
+++ 10. python file handling.py
@@ -8,14 +8,26 @@
 
 # Main
 
+import os   # need this module to delete the file
+
+print("Opening testfile.txt...")
 FILE = open("testfile.txt", "w")    # open testfile.txt for write access, create it if needed
 
 # write three lines to file
+print("Writing three lines of text to testfile.txt...")
 for i in range(0, 3):
     FILE.write("Writing line " + str(i) + "\n")
 
 # reopen the file for read access
+print("The first line is:")
 FILE = open("testfile.txt", "r")
-print(FILE.readline(), end="")  # read and print the first line, don't add a newline
+print("\t" + FILE.readline(), end="")  # read and print the first line, don't add a newline
 
+# wait for user input
+input("Press any key to delete the file and exit: ")
+
+print("Deleting testfile.txt...")
+os.remove("testfile.txt")   # nuke the file
+
+print("DONE")
 # End
