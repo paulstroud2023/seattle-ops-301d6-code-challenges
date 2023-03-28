c8f7cbccca6f4821e41cbad20ae5d34b333014c8
Paul Stroud <paul.stroud2023@gmail.com>
2023-03-28 05:47:32 +0000

    comments and tweaks

--- 11. psutil.py
+++ 11. psutil.py
@@ -8,21 +8,24 @@
 
 # Main
 
-import psutil
+import psutil   # import the psutil module
 
-# Get the CPU times
+# save the return values' array from psutil.cpu_times()
 cpu_times = psutil.cpu_times()
 
-# Print the desired CPU time information
-print(f"Time spent by normal processes executing in user mode: {cpu_times.user:.2f} seconds")
-print(f"Time spent by processes executing in kernel mode: {cpu_times.system:.2f} seconds")
-print(f"Time when system was idle: {cpu_times.idle:.2f} seconds")
-print(f"Time spent by priority processes executing in user mode: {cpu_times.nice:.2f} seconds")
-print(f"Time spent waiting for I/O to complete: {cpu_times.iowait:.2f} seconds")
-print(f"Time spent for servicing hardware interrupts: {cpu_times.irq:.2f} seconds")
-print(f"Time spent for servicing software interrupts: {cpu_times.softirq:.2f} seconds")
-print(f"Time spent by other operating systems running in a virtualized environment: {cpu_times.steal:.2f} seconds")
-print(f"Time spent running a virtual CPU for guest operating systems under the control of the Linux kernel: {cpu_times.guest:.2f} seconds")
+# print specific times with a corresponding prompt
+# formatting output as f-strings that include the array/variable
+print(f"Time spent by normal processes executing in user mode: {cpu_times.user} sec")
+print(f"Time spent by processes executing in kernel mode: {cpu_times.system} sec")
+print(f"Time when system was idle: {cpu_times.idle} sec")
+print(f"Time spent by priority processes executing in user mode: {cpu_times.nice} sec")
+print(f"Time spent waiting for I/O to complete: {cpu_times.iowait} sec")
+print(f"Time spent for servicing hardware interrupts: {cpu_times.irq} sec")
+print(f"Time spent for servicing software interrupts: {cpu_times.softirq} sec")
+print(f"Time spent by other operating systems running in a virtualized environment: {cpu_times.steal} sec")
+print(f"Time spent running a virtual CPU for guest operating systems under the control of the Linux kernel: {cpu_times.guest} sec")
 
+
+print("\nDONE")
 
 # End
