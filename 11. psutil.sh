e287d14b886e9442a2a564924b7a5a6b0280a75a
Paul Stroud <paul.stroud2023@gmail.com>
2023-03-28 05:21:29 +0000

    initial upload

---
+++ 11. psutil.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/python 
+
+# Script: Ops 301 Class 11 Ops Challenge Solution
+# Author: Paul Stroud
+# Date of latest revision: 03/27/23
+# Purpose: Python psutil
+
+
+# Main
+
+
+
+# End
